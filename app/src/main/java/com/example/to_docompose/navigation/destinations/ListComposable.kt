81aac460d5cc5f2b2e1297c8443ddf7dc975362c
yanivkr7 <yanivkr7@gmail.com>
2022-05-09 18:33:19 +0300

    Nav

---
+++ app/src/main/java/com/example/to_docompose/navigation/destinations/ListComposable.kt
@@ -0,0 +1,19 @@
+package com.example.to_docompose.navigation.destinations
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.example.to_docompose.util.Constants.LIST_ARGUMENT_KEY
+import com.example.to_docompose.util.Constants.LIST_SCREEN
+
+fun NavGraphBuilder.listComposable(){
+    composable(
+        route = LIST_SCREEN,
+        arguments = listOf(navArgument(LIST_ARGUMENT_KEY){
+            type = NavType.StringType
+        })
+    ){
+
+    }
+}
---
+++ app/src/main/java/com/example/to_docompose/util/Action.kt
@@ -0,0 +1,11 @@
+package com.example.to_docompose.util
+
+enum class Action {
+    ADD,
+    UPDATE,
+    DELETE,
+    DELETE_ALL,
+    UNDO,
+    NO_ACTION
+
+}
